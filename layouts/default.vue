8dde0c1c1c738318abb0180c6a611ab8744571d1
Thorsten Luenborg <t.luenborg@googlemail.com>
2021-06-22 10:28:18 +0200

    styles: extract default background to main css

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,20 +2,11 @@
 import props from '../utils/props'
 export default {
   props,
-  setup() {
-  },
+  setup() {},
 }
 </script>
 <template>
   <div class="slidev-layout default">
-    <slot/>
+    <slot />
   </div>
 </template>
-<style lang="postcss">
-.slidev-layout.default {
-  background-image: url('/bg-default.svg');
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-position: center center;
-}
-</style>
